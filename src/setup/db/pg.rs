dc5be0af5c1cbbf3d3b5970a45bc7f7b53694374
EmmaWedi <aniagyeieo@gmail.com>
2025-04-11 18:40:09 +0100

    adding helpful tips for production

--- src/setup/db/pg.rs
+++ src/setup/db/pg.rs
@@ -26,13 +26,13 @@ pub async fn pg_conn(settings: &Config) -> DatabaseConnection {
 
     options
         .max_connections(max)
-        .min_connections(5)
-        .connect_timeout(Duration::from_secs(8))
-        .acquire_timeout(Duration::from_secs(8))
+        .min_connections(10)
+        .connect_timeout(Duration::from_secs(5))
+        .acquire_timeout(Duration::from_secs(10))
         .idle_timeout(Duration::new(idle_timeout, 0))
-        .max_lifetime(Duration::from_secs(8))
-        .sqlx_logging(true)
-        .sqlx_logging_level(log::LevelFilter::Info);
+        .max_lifetime(Duration::from_secs(3600))
+        .sqlx_logging(true) //for prod change this to false
+        .sqlx_logging_level(log::LevelFilter::Info); //for prod change the Info to Off
 
     let pool = Database::connect(options).await.unwrap();
 
